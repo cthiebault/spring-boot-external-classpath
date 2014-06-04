5b0eb199071badeb49f585c8128e8e3783f8fcb9
Cedric Thiebault <cthiebault@p3g.org>
2014-06-04 11:41:15 -0400

    Internal property works

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,27 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+  @Value("${internal.property}")
+  private String internal;
+
+//  @Value("${external.property}")
+  private String external;
+
+  @RequestMapping("/greeting")
+  public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+      Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("internal", internal);
+    model.addAttribute("external", external);
+    return "greeting";
+  }
+
+}
